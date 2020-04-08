ac9374d9dde2ba9292d7492afbaa044f48a50809
Abdelrahman Sameh <asameh@inmobly.com>
2020-04-08 17:15:32 +0200

    move session_scope in roles_api to roles_repository

--- oauth/api/roles_api.py
+++ oauth/api/roles_api.py
@@ -1,24 +1,21 @@
 from flask import Response, request
-from gateway.db import session_scope
 from repositories.roles import RoleRepository
 from utils import json
 from http import HTTPStatus
-from utils.validation_helper import __is_duplicated_data
 
 
 def create_role(app_id):
     try:
         role_repository = RoleRepository()
-        with session_scope() as session:
-            data = request.get_json()
-            is_duplicated, error_message = __is_duplicated_data(session, role_repository, app_id, data)
-            if is_duplicated:
-                return Response(response=json.dumps({"errors": error_message}),
-                                status=HTTPStatus.CONFLICT.value, mimetype='application/json')
-            data['app_id'] = app_id
-            role = role_repository.create(session, **data)
-            return Response(response=json.dumps(role),
-                            status=HTTPStatus.CREATED.value, mimetype='application/json')
+        data = request.get_json()
+        is_duplicated, error_message = role_repository.is_duplicated_data(app_id, data)
+        if is_duplicated:
+            return Response(response=json.dumps({"errors": error_message}),
+                            status=HTTPStatus.CONFLICT.value, mimetype='application/json')
+        data['app_id'] = app_id
+        role = role_repository.create(**data)
+        return Response(response=json.dumps(role),
+                        status=HTTPStatus.CREATED.value, mimetype='application/json')
 
     except Exception as err:
         if 'message' not in err.__dict__:
@@ -31,17 +28,16 @@ def create_role(app_id):
 def update_role(app_id, role_id):
     try:
         role_repository = RoleRepository()
-        with session_scope() as session:
-            data = request.get_json()
+        data = request.get_json()
 
-            is_duplicated, error_message = __is_duplicated_data(session, role_repository, app_id, data, role_id)
-            if is_duplicated:
-                return Response(response=json.dumps({"errors": error_message}),
-                                status=HTTPStatus.CONFLICT.value, mimetype='application/json')
+        is_duplicated, error_message = role_repository.is_duplicated_data(app_id, data, role_id)
+        if is_duplicated:
+            return Response(response=json.dumps({"errors": error_message}),
+                            status=HTTPStatus.CONFLICT.value, mimetype='application/json')
 
-            role = role_repository.update(session, role_id, app_id, **data)
-            return Response(response=json.dumps(role),
-                            status=HTTPStatus.ACCEPTED.value, mimetype='application/json')
+        role = role_repository.update(role_id, app_id, **data)
+        return Response(response=json.dumps(role),
+                        status=HTTPStatus.ACCEPTED.value, mimetype='application/json')
 
     except Exception as err:
         if 'message' not in err.__dict__:
@@ -54,10 +50,9 @@ def update_role(app_id, role_id):
 def list_roles(app_id):
     try:
         role_repository = RoleRepository()
-        with session_scope() as session:
-            roles = role_repository.list(session, app_id)
-            return Response(response=json.dumps(roles),
-                            status=HTTPStatus.OK, mimetype='application/json')
+        roles = role_repository.list(app_id)
+        return Response(response=json.dumps(roles),
+                        status=HTTPStatus.OK, mimetype='application/json')
 
     except Exception as err:
         if 'message' not in err.__dict__:
@@ -70,10 +65,9 @@ def list_roles(app_id):
 def delete_role(app_id, role_id):
     try:
         role_repository = RoleRepository()
-        with session_scope() as session:
-            role_repository.delete(session, role_id, app_id)
-            return Response(response="",
-                            status=HTTPStatus.NO_CONTENT, mimetype='application/json')
+        role_repository.delete(role_id, app_id)
+        return Response(response="",
+                        status=HTTPStatus.NO_CONTENT, mimetype='application/json')
 
     except Exception as err:
         if 'message' not in err.__dict__:
