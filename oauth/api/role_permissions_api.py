424c7f88adc89f6780a72a264debd8bc3b8d8b18
Abdelrahman Sameh <asameh@inmobly.com>
2020-04-25 17:32:47 +0200

    send app_id to add permission action

--- oauth/api/role_permissions_api.py
+++ oauth/api/role_permissions_api.py
@@ -1,5 +1,4 @@
 from flask import Response, request
-from gateway.db import session_scope
 from repositories.role_permission import RolePermissionRepository
 from utils import json
 from http import HTTPStatus
@@ -10,7 +9,7 @@ def add_permission(app_id, role_id):
         role_permission_repository = RolePermissionRepository()
         data = request.get_json()
         data['role_id'] = role_id
-        role_permission = role_permission_repository.create(**data)
+        role_permission = role_permission_repository.create(app_id, **data)
         return Response(response=json.dumps(role_permission),
                         status=HTTPStatus.OK.value, mimetype='application/json')
 
@@ -40,7 +39,7 @@ def get_permissions(app_id, role_id):
 def delete_permission(app_id, role_id, permission_id):
     try:
         role_permission_repository = RolePermissionRepository()
-        role_permission = role_permission_repository.delete(permission_id)
+        role_permission = role_permission_repository.delete(app_id, role_id, permission_id)
         return Response(response="",
                         status=HTTPStatus.NO_CONTENT.value, mimetype='application/json')
 
